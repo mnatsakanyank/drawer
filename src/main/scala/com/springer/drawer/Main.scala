27c2bb6622b91b400067019160aa059bb6bf5673
karen.mnatsakanyan@kaufda.de <karen.mnatsakanyan@kaufda.de>
2017-05-01 14:57:02 +0200

    Gradle build fixes

--- src/main/scala/com/springer/drawer/Main.scala
+++ src/main/scala/com/springer/drawer/Main.scala
@@ -6,29 +6,27 @@ import com.springer.drawer.action.{Action, Help}
 import com.springer.drawer.command.{Canvas, Command}
 import com.springer.drawer.command.CommandFactory._
 
-object Drawer {
+object Main extends App {
 
   val scanner: Scanner = new java.util.Scanner(System.in).useDelimiter("[\\r\\n]+")
 
-  def main(args: Array[String]): Unit = {
 
-    var canvas: Canvas = null
-    while (canvas == null) {
-      println("Please insert Canvas command")
-      val command = askForCommand()
-      if (command.isDefined && command.get.isInstanceOf[Canvas]) {
-        canvas = command.get.asInstanceOf[Canvas]
-      }
+  var canvas: Canvas = _
+  while (canvas == null) {
+    println("Please insert Canvas command")
+    val command = askForCommand()
+    if (command.isDefined && command.get.isInstanceOf[Canvas]) {
+      canvas = command.get.asInstanceOf[Canvas]
     }
+  }
 
-    while (true) {
-      println("Please insert command")
-      val command = askForCommand()
-      if (command.isDefined) {
-        canvas += command.get
-        canvas.draw(canvas)
-        println()
-      }
+  while (true) {
+    println("Please insert command")
+    val command = askForCommand()
+    if (command.isDefined) {
+      canvas += command.get
+      canvas.draw(canvas)
+      println()
     }
   }
 
